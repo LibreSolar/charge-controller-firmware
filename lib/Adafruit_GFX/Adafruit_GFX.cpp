6b0c5f3c971e71fe451418bb154bbea5468c7913
Hogthrob <hogthrob@github.com>
2018-10-06 17:43:33 +0200

    Fixed issue in Adafruit_GFX lib Well, gcc was right with its warnings (code was incorrect).

--- lib/Adafruit_GFX/Adafruit_GFX.cpp
+++ lib/Adafruit_GFX/Adafruit_GFX.cpp
@@ -270,13 +270,19 @@ void Adafruit_GFX::fillTriangle ( int16_t x0, int16_t y0, int16_t x1, int16_t y1
     
     // Sort coordinates by Y order (y2 >= y1 >= y0)
     if (y0 > y1)
+    {
         swap(y0, y1); swap(x0, x1);
+    }
 
     if (y1 > y2)
+    {
         swap(y2, y1); swap(x2, x1);
+    }
 
     if (y0 > y1)
+    {
         swap(y0, y1); swap(x0, x1);
+    }
 
     
     if(y0 == y2)
