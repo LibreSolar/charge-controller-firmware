89ea8827fb2d959197cae3f5f6ec6a73dab1fd31
Hogthrob <hogthrob@github.com>
2019-06-22 09:56:01 +0200

    Fixed Load_A not correctly reported issue
    At compile time &(load.bus->current) will resolve to a different
    (wrong) address than after assignment of "load_bus" to load.bus at runtime.

--- src/data_objects.cpp
+++ src/data_objects.cpp
@@ -131,7 +131,7 @@ const data_object_t data_objects[] = {
     {0x70, TS_OUTPUT, TS_READ_ALL, TS_T_FLOAT32, 2, (void*) &(ls_bus.voltage),                  "Bat_V"},
     {0x71, TS_OUTPUT, TS_READ_ALL, TS_T_FLOAT32, 2, (void*) &(hs_bus.voltage),                  "Solar_V"},
     {0x72, TS_OUTPUT, TS_READ_ALL, TS_T_FLOAT32, 2, (void*) &(ls_bus.current),                  "Bat_A"},
-    {0x73, TS_OUTPUT, TS_READ_ALL, TS_T_FLOAT32, 2, (void*) &(load.bus->current),               "Load_A"},
+    {0x73, TS_OUTPUT, TS_READ_ALL, TS_T_FLOAT32, 2, (void*) &(load_bus.current),                "Load_A"},
     {0x74, TS_OUTPUT, TS_READ_ALL, TS_T_FLOAT32, 1, (void*) &(charger.bat_temperature),         "Bat_degC"},
     {0x75, TS_OUTPUT, TS_READ_ALL, TS_T_BOOL,    1, (void*) &(charger.ext_temp_sensor),         "BatTempExt"},
     {0x76, TS_OUTPUT, TS_READ_ALL, TS_T_FLOAT32, 1, (void*) &(mcu_temp),                        "MCU_degC"},
